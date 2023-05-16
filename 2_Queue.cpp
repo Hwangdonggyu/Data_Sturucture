a5d831a15996c71a94d75e93248e167f1c95efec
황동규 <121680372+Hwangdonggyu@users.noreply.github.com>
2023-05-16 15:21:48 +0900

    Realize 2_Queue.cpp
    Realize 2_Queue.cpp

--- 2_Queue.cpp
+++ 2_Queue.cpp
@@ -3,12 +3,43 @@
 #include <ctype.h>
 #define max_queue 100
 
-#ifndef __cplusplus
-typedef enum { false, true }bool;
-#endif
 typedef int data;
 typedef struct {
-	int fornt, rear;
+	int front, rear;
 	data items[max_queue];
 
 }queue;
+
+void initqueue(queue* pqueue);
+bool isfull(queue* pqueue);
+bool isempty(queue* pqueue);
+data peek(queue* pqueue);
+void enqueue(queue* pqueue, data item);
+void dequeue(queue* pqueue);
+
+int main(){
+
+}
+
+void initqueue(queue* pqueue){
+	pqueue->front = pqueue-> rear = 0;
+}
+bool isfull(queue* pqueue){
+	return pqueue->front == (pqueue->rear+1)%max_queue;
+}
+bool isempty(queue* pqueue){
+	return pqueue->front == pqueue->rear;
+}
+data peek(queue* pqueue){
+	if(isempty(pqueue)) exit(1);
+	return pqueue->items[pqueue->front];
+}
+void enqueue(queue* pqueue, data item){
+	if(isfull(pqueue)) exit(1);
+	pqueue->items[pqueue->rear]= item;
+	pqueue->rear = (pqueue->rear+1) % max_queue;
+}
+void dequeue(queue* pqueue){
+	if(isempty(pqueue)) exit(1);
+	pqueue->front = (pqueue->front+1)% max_queue;
+}
